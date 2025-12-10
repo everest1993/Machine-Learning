0bfeb6d45128cac7683dca682ded31ed622d2be5
everest1993 <luigi.vannozzi@protonmail.com>
2025-12-10 11:47:24 +0100

    numpy implementation

--- Supervised Machine Learning/multiple_linear_regression.py
+++ Supervised Machine Learning/multiple_linear_regression.py
@@ -1,5 +1,80 @@
+"""
+Logica per il calcolo della regressione lineare multipla
+- compute_gradient per il calcolo del gradiente dati i valori w e b
+- gradient_descent per eseguire il metodo di discesa del gradiente e individuare i minimi
+"""
+
+from typing import Tuple
 import numpy as np
 
-X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
-y_train = np.array([460, 232, 178])
 
+def compute_gradient(
+        X: np.ndarray, 
+        y: np.ndarray, 
+        w: np.ndarray, 
+        b: float
+        ) -> Tuple[np.ndarray, float]:
+    """
+    Calcola il gradiente per la regressione lineare
+    Parametri:
+        X: input (matrice di dimensione m x n)
+        y: array di valori effettivi
+        w: array contenente i coefficienti angolari
+        b: intercetta
+    Restituisce:
+        dj_dw: array con gradienti per i parametri w
+        dj_db: gradiente per il parametro b
+    """
+    m, n = X.shape
+
+    dj_dw = np.zeros(n)
+    dj_db = 0.0
+
+    for i in range(m): # per ogni riga
+        err = (np.dot(X[i], w) + b) - y[i] # errore calcolato sul vettore
+
+        for j in range(n): # per ogni colonna
+            dj_dw[j] += err * X[i][j] # calcolo gradiente con derivata
+        
+        dj_db += err
+
+    dj_dw /= m
+    dj_db /= m
+
+    return dj_dw, dj_db
+
+
+def gradient_descent(
+        X: np.ndarray, 
+        y: np.ndarray, 
+        w_in: np.ndarray, 
+        b_in: float, 
+        alpha: float, 
+        num_iters: int, 
+        gradient_function
+        ) -> Tuple[np.ndarray, float]: 
+    """
+    Esegue il metodo di discesa del gradiente per i parametri w e b. 
+    Aggiorna w, b effettuando num_iters steps con learning rate alpha
+    Parametri:
+        X: input (matrice m x n)
+        y: array di valori effettivi
+        w_in: array con valori iniziali di w
+        b_in: valore iniziale di b
+        alpha: learning rate
+        num_iters: numero di iterazioni per calcolare la discesa del gradiente
+        gradient_function: funzione per il calcolo del gradiente
+    Restituisce:
+        w: array di punti di minimo per il coefficiente angolare
+        b: punto di minimo per l'intercetta
+    """
+    w = np.copy(w_in)
+    b = b_in
+
+    for _ in range(num_iters):
+        dj_dw, dj_db = gradient_function(X, y, w, b)
+        
+        w -= alpha * dj_dw
+        b -= alpha * dj_db
+
+    return w, b
--- Supervised Machine Learning/univariate_linear_regression.py
+++ Supervised Machine Learning/univariate_linear_regression.py
@@ -4,40 +4,49 @@ Logica per il calcolo della regressione lineare univariata:
 - gradient_descent per eseguire il metodo di discesa del gradiente e individuare i minimi
 """
 
-def compute_gradient(x, y, w, b):
+from typing import Tuple
+import numpy as np
+
+
+def compute_gradient(
+        x: np.ndarray, 
+        y: np.ndarray, 
+        w: float, 
+        b: float
+        ) -> Tuple[float, float]:
     """
     Calcola il gradiente per la regressione lineare
     Parametri:
-        x : input (m elementi)
-        y : valori effettivi
+        x: input (m elementi)
+        y: valori effettivi
         w, b: coefficiente angolare e intercetta
     Restituisce:
         dj_dw: gradiente per il parametro w
         dj_db: gradiente per il parametro b
     """
     m = x.shape[0]
-    dj_dw = 0 # derivata parziale rispetto a w
-    dj_db = 0 # derivata parziale rispetto a b
+    dj_dw = 0.0 # derivata parziale rispetto a w
+    dj_db = 0.0 # derivata parziale rispetto a b
 
     for i in range(m):
         f_wb = w * x[i] + b # applicazione del modello lineare
 
         # gradienti come derivate dell'errore quadratico medio
-        dj_dw_i = (f_wb - y[i]) * x[i] 
-        dj_db_i = f_wb - y[i]
+        dj_dw += np.dot((f_wb - y[i]), x[i])
+        dj_db += f_wb - y[i]
 
-        dj_dw += dj_dw_i
-        dj_db += dj_db_i
-
-    # i gradienti rappresentano la pendenza della superficie di costo J(w, b)
-    # per individuare il minimo è necessario muoversi sulla superficie in direzione contraria
-    dj_dw = dj_dw / m
-    dj_db = dj_db / m
-
-    return dj_dw, dj_db
+    return dj_dw / m, dj_db / m
 
 
-def gradient_descent(x, y, w_in, b_in, alpha, num_iters, gradient_function): 
+def gradient_descent(
+        x: np.ndarray, 
+        y: np.ndarray, 
+        w_in: float, 
+        b_in: float, 
+        alpha: float, 
+        num_iters: int, 
+        gradient_function
+        ) -> Tuple[float, float]: 
     """
     Esegue il metodo di discesa del gradiente per i parametri w e b. 
     Aggiorna w, b effettuando num_iters steps con learning rate alpha
